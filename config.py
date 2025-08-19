17f43fe18c22c76ec23e6ce3457c8c6866a11664
Byter <vitazagrebelnyh@gmail.com>
2025-08-19 20:13:07 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from decouple import config
 
 BOT_TOKEN = config('BOT_TOKEN')
-WEBHOOK_HOST = config("WEBHOOK_HOST")
-WEBHOOK_PATH = f"/webhook"
-WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
+WEBHOOK_HOST = config('WEBHOOK_HOST')
+WEBHOOK_PATH = f'/webhook'
+WEBHOOK_URL = f'https://{WEBHOOK_HOST}{WEBHOOK_PATH}'
+PORT = config('PORT')
