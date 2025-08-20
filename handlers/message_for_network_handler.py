e37d2b45f40dc7f785f8beaa64271be93d85d3f5
Byter <vitazagrebelnyh@gmail.com>
2025-08-20 19:44:05 +0900

    add context

--- handlers/message_for_network_handler.py
+++ handlers/message_for_network_handler.py
@@ -22,8 +22,10 @@ async def simple_message_handler(message: Message):
             await message.answer("Думаю над твоим вопросом...")
             if user.end_subscription_day.date() <= datetime.now().date():
                 user.gpt_4o_mini_requests -= 1
-                await db_repo.update_user(user)
-            await message.answer(gpt.chat_with_gpt4o_mini(message.text))
+            reply, new_context = gpt.chat_with_gpt4o_mini(message.text, user.context)
+            await message.answer(reply)
+            user.context = new_context
+            await db_repo.update_user(user)
         case 1:
             if user.end_subscription_day.date() <= datetime.now().date() or user.gpt_5_requests < 1:
                 await message.answer("Кажется твои запросы на сегодня уже закончились:( "
@@ -31,8 +33,10 @@ async def simple_message_handler(message: Message):
                 return
             await message.answer("Думаю над твоим вопросом...")
             user.gpt_5_requests -= 1
+            reply, new_context = gpt.chat_with_gpt5(message.text, user.context)
+            await message.answer(reply)
+            user.context = new_context
             await db_repo.update_user(user)
-            await message.answer(gpt.chat_with_gpt5(message.text))
         case _:
             logging.info(f"Текущая нейронка {user.current_neural_network}")
 
--- neural_networks/gpt.py
+++ neural_networks/gpt.py
@@ -1,33 +1,39 @@
 import logging
+from typing import Optional, List, Dict, Tuple
 
 
 class GPT:
     def __init__(self, openai):
         self.openai = openai
 
-    def chat_with_gpt4o_mini(self, message_text):   
+    def chat_with_gpt4o_mini(self, message_text: str, context: Optional[List[Dict]]) -> Tuple:   
         try:
+            message = {"role": "user", "content": message_text if message_text else ""}
             response = self.openai.chat.completions.create(
                 model="gpt-4o-mini",
-                messages=[{"role": "user", "content": message_text if message_text else ""}],
+                messages=[message],
                 temperature=0.7
             )
             reply = response.choices[0].message.content
-            # logging.info(f"reply - {reply}\n\nresponse - {response}")
-            return reply
+            context.append(message)
+            context.append({"role": "assistant", "content": reply})
+            return (reply, context)
 
         except Exception as e:
             logging.error(f"Ошибка GPT 4o mini {e}")
-            return (str(e))
+            return (str(e), [])
         
-    def chat_with_gpt5(self, message_text):
+    def chat_with_gpt5(self, message_text: str, context: Optional[List[Dict]]) -> str:
         try:
+            message = {"role": "user", "content": message_text if message_text else ""}
             response = self.openai.chat.completions.create(
                 model="gpt-5",
                 messages=[{"role": "user", "content": message_text if message_text else ""}]
             )
             reply = response.choices[0].message.content
-            return reply
+            context.append(message)
+            context.append({"role": "assistant", "content": reply})
+            return (reply, context)
         except Exception as e:
             logging.error(f"Ошибка GPT 5 {e}")
-            return (str(e))
+            return (str(e), [])
