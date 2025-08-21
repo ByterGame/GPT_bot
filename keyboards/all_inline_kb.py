1e06b94985ecbf6f3792ddc7c27d014be64ca5a0
Byter <vitazagrebelnyh@gmail.com>
2025-08-21 14:36:26 +0900

    Update all_inline_kb.py

--- keyboards/all_inline_kb.py
+++ keyboards/all_inline_kb.py
@@ -4,9 +4,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def set_mode_kb():
     kb_list = [
-        [InlineKeyboardButton(text="gpt-4o mini", callback_data="set_gpt_4o_mini")],
-        [InlineKeyboardButton(text="gpt-5 full", callback_data="set_gpt5_full")],
-        [InlineKeyboardButton(text="gpt-5 vision", callback_data="set_gpt5_vision")]
+        [InlineKeyboardButton(text="gpt-4o mini", callback_data="set_gpt_4o_mini"), InlineKeyboardButton(text="gpt-5 full", callback_data="set_gpt5_full")],
+        [InlineKeyboardButton(text="gpt-5 vision", callback_data="set_gpt5_vision"), InlineKeyboardButton(text="DALL·E", callback_data="set_dalle")]
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
     return keyboard
