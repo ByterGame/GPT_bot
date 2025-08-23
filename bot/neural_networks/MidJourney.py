c4602834bc8800811022e317489a7e15b20ec2ba
Byter <vitazagrebelnyh@gmail.com>
2025-08-23 15:30:05 +0900

    Update MidJourney.py

--- bot/neural_networks/MidJourney.py
+++ bot/neural_networks/MidJourney.py
@@ -1,7 +1,7 @@
 import requests
 from config import DISCORD_CHANNEL_ID, DISCORD_SERVER_ID, DISCORD_TOKEN
 
-MJ_PROXY_URL = "http://localhost:8081/mj"
+MJ_PROXY_URL = "http://localhost:8081"
 
 async def send_prompt(prompt: str):
     headers = {
