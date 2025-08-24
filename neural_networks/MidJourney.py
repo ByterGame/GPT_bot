8245ae565b587094242199d73bc1cd6a9b129171
Byter <vitazagrebelnyh@gmail.com>
2025-08-24 18:06:59 +0900

    add MJ_KEY

--- neural_networks/MidJourney.py
+++ neural_networks/MidJourney.py
@@ -1,27 +1,52 @@
 import requests
-from config import DISCORD_CHANNEL_ID, DISCORD_SERVER_ID, DISCORD_USER_TOKEN
+import time
+from config import MJ_KEY
 
-MJ_PROXY_URL = "http://localhost:8081/mj"
 
-async def send_prompt(prompt: str):
-    headers = {
-        "Authorization": f"Bearer {DISCORD_USER_TOKEN}", 
-        "Content-Type": "application/json"
+url = "https://api.goapi.ai/api/v1/task"
+headers = {
+    "X-API-KEY": MJ_KEY,
+    "Content-Type": "application/json"
+}
+payload = {
+    "model": "midjourney",
+    "task_type": "imagine",
+    "input": {
+        "prompt": "realistic bmw drifting in desert",
+        "aspect_ratio": "16:9",
+        "process_mode": "",
+        "skip_prompt_check": True,
+        "bot_id": 0
+    },
+    "config": {
+        "service_mode": "",
+        "webhook_config": {
+            "endpoint": "",
+            "secret": ""
+        }
     }
+}
 
-    data = {
-        "prompt": prompt,
-        "guildId": DISCORD_SERVER_ID,
-        "channelId": DISCORD_CHANNEL_ID
-    }
+resp = requests.post(url, headers=headers, json=payload)
+if resp:
+    data = resp.json()
+    print("Ответ imagine:", data)
+    task_id = data.get("data", {}).get("task_id")
+    if not task_id:
+        raise Exception("Не удалось получить task_id")
 
-    try:
-        response = requests.post(f"{MJ_PROXY_URL}/submit/imagine", headers=headers, json=data, timeout=10)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        return {"error": f"Ошибка запроса: {e}"}
+    while True:
+        time.sleep(5)
+        fetch_resp = requests.get(f"{url}/{task_id}", headers=headers)
+        status_data = fetch_resp.json().get("data", {})
+        status = status_data.get("status")
+        print("Статус задачи:", status)
 
-    try:
-        return response.json()
-    except ValueError:
-        return {"error": f"Ответ сервера не JSON: {response.text}"}
+        if status in ("finished", "success", "completed"):
+            image_url = status_data.get("output", {}).get("image_url")
+            print("Итоговая картинка:", image_url)
+            break
+else: 
+    print(f"resp: {resp} is empty")
+
+
