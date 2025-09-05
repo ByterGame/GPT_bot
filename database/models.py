dd43683b359665e6e62fb56c88402905d04fcf11
Byter <vitazagrebelnyh@gmail.com>
2025-09-05 21:27:48 +0900

    Update models.py

--- database/models.py
+++ database/models.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional, List, Dict
 import pytz
-from config import DEFAULT_GPT_4O_LIMIT, DEFAULT_PROMPT
+from config import DEFAULT_PROMPT
 
 class User(BaseModel):
     id: int
     context: Optional[List[Dict]] = [{"role": "system", "content": DEFAULT_PROMPT}]
-    gpt_4o_mini_requests: int = DEFAULT_GPT_4O_LIMIT
+    gpt_4o_mini_requests: int = 30
     balance: int = 0
     current_neural_network: int = 0
     with_bonus: bool = False
